18cdc5f2977bb0d29441f505c8b5fa2ce85d8be8
Xiang Tang <tangxiang1981@gmail.com>
2019-03-02 15:52:06 -0500

    first commit

---
+++ src/main/java/ca/alextang/usermanagement/controller/UserController.java
@@ -0,0 +1,34 @@
+package ca.alextang.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserController {
+	
+	@GetMapping
+	public String getUser() {
+		return "get user";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "create user";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user";
+	}
+	
+}
